9b95538f7e07936bd6d72412e1911617fbc0c6bc
Tomasz Puchacz <tpuchacz1@gmail.com>
2025-02-18 21:19:24 +0100

    Added templates, to fix picking multiple of the same software item in them

--- PracaInzynierska/MainCode.cs
+++ PracaInzynierska/MainCode.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using HandyControl.Tools.Extension;
 using Microsoft.Data.SqlClient;
 using Microsoft.Win32;
 using System.Collections;
@@ -10,6 +11,7 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Windows.Input;
 
@@ -26,7 +28,16 @@ namespace PracaInzynierska
         private bool canClickInstallButton = false;
 
         [ObservableProperty]
+        private bool enableControls = true;
+
+        [ObservableProperty]
         private bool operationInProgress = false;
+
+        [ObservableProperty]
+        private bool templatesShown = false;
+
+        [ObservableProperty]
+        private bool programsShown = true;
 
         [ObservableProperty]
         private ObservableCollection<InstalledApplications> appList;
@@ -42,11 +53,19 @@ namespace PracaInzynierska
         private bool afterInstalling = false;
 
         [ObservableProperty]
+        private ObservableCollection<SoftwareItem> currentSoftwareItems;
+
+        //[ObservableProperty]
         private ObservableCollection<SoftwareItem> softwareItems;
+
+        //[ObservableProperty]
+        private ObservableCollection<SoftwareItem> templateItems;
         public MainCode()
         {
             AppList = new ObservableCollection<InstalledApplications>();
-            SoftwareItems = new ObservableCollection<SoftwareItem>();
+            softwareItems = new ObservableCollection<SoftwareItem>();
+            templateItems = new ObservableCollection<SoftwareItem>();
+            CurrentSoftwareItems = new ObservableCollection<SoftwareItem>();
             FetchSoftwareItems();
             client = new HttpClient();
             SelectedSoftwareItems = new ObservableCollection<SoftwareItem>();
@@ -56,7 +75,7 @@ namespace PracaInzynierska
         private void FetchSoftwareItems()
         {
             ListPrograms();
-            SoftwareItems.Clear();
+            softwareItems.Clear();
 
             using (SqlConnection _con = new SqlConnection(connectionString))
             {
@@ -72,24 +91,10 @@ namespace PracaInzynierska
                         while (reader.Read())
                         {
                             var item = new SoftwareItem();
-                            item.SoftwareId = reader.GetInt32("softwareId");
-                            item.Name = reader.GetString("name");
-                            item.Version = reader.GetString("currentVersion");
-                            item.WebsiteLink = reader.GetString("websiteLink");
-                            item.DownloadLink = reader.GetString("downloadLink");
-                            item.Creator = reader.GetString("companyName");
-                            item.Version = reader.GetString("currentVersion");
-                            item.LastUpdate = reader.GetDateTime("updateDate").ToString("dd MMMM, yyyy");
-                            item.Category = reader.GetString("category");
-                            item.ParameterSilent = reader.GetString("parameterSilent");
-                            item.ParameterDirectory = reader.GetString("parameterDir");
-                            InstalledApplications installedApp = InstalledApplications.FindApp(item.Name, AppList);
-                            if (installedApp != null)
-                                item.CurrentVersion = installedApp.Version;
-                            item.DownloadCount = reader.GetInt32("downloadCount");
-                            SoftwareItems.Add(item);
+                            softwareItems.Add(ReadSoftwareItem(reader, false, ""));
                         }
                     }
+
                 }
                 catch (Exception ex)
                 { 
@@ -97,7 +102,97 @@ namespace PracaInzynierska
                 }
                 finally { _con.Close(); }
             }
-            OnPropertyChanged(nameof(SoftwareItems));
+            LoadTemplates();
+            CurrentSoftwareItems = softwareItems.Clone();
+            OnPropertyChanged(nameof(CurrentSoftwareItems));
+        }
+
+        [RelayCommand]
+        private void ShowTemplates()
+        {
+            if(!TemplatesShown)
+            {
+                CurrentSoftwareItems = templateItems.Clone();
+                OnPropertyChanged(nameof(CurrentSoftwareItems));
+                TemplatesShown = true;
+                ProgramsShown = false;
+            }
+        }
+
+        [RelayCommand]
+        private void ShowProgramList()
+        {
+            if (!ProgramsShown)
+            {
+                ProgramsShown = true;
+                TemplatesShown = false;
+                CurrentSoftwareItems = softwareItems.Clone();
+            }
+        }
+
+        private SoftwareItem ReadSoftwareItem(SqlDataReader reader, bool isTemplate, string templateName)
+        {
+            var item = new SoftwareItem();
+            item.SoftwareId = reader.GetInt32("softwareId");
+            item.Name = reader.GetString("name");
+            item.Version = reader.GetString("currentVersion");
+            item.WebsiteLink = reader.GetString("websiteLink");
+            item.DownloadLink = reader.GetString("downloadLink");
+            item.Creator = reader.GetString("companyName");
+            item.Version = reader.GetString("currentVersion");
+            item.LastUpdate = reader.GetDateTime("updateDate").ToString("dd MMMM, yyyy");
+            if(isTemplate)
+                item.Category = templateName;
+            else
+                item.Category = reader.GetString("category");
+            item.ParameterSilent = reader.GetString("parameterSilent");
+            item.ParameterDirectory = reader.GetString("parameterDir");
+            InstalledApplications installedApp = InstalledApplications.FindApp(item.Name, AppList);
+            if (installedApp != null)
+                item.CurrentVersion = installedApp.Version;
+            item.DownloadCount = reader.GetInt32("downloadCount");
+            return item;
+        }
+
+        private void LoadTemplates()
+        {
+            using (SqlConnection _con = new SqlConnection(connectionString))
+            {
+                string queryStatement = "SELECT * FROM dbo.templates";
+
+                try
+                {
+                    _con.Open();
+                    List<string> templateNames = new List<string>();
+                    List<string> procedureNames = new List<string>();
+                    using (var cmd = new SqlCommand(queryStatement, _con))
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            templateNames.Add(reader.GetString("templateName"));
+                            procedureNames.Add(reader.GetString("procedureName"));
+                        }
+                    }
+
+                    for (int i = 0; i < procedureNames.Count; i++)
+                    {
+                        using (var cmd = new SqlCommand(procedureNames[i], _con))
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                templateItems.Add(ReadSoftwareItem(reader, true, templateNames[i]));
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ProgressText = "Wystąpił błąd:\n" + ex.Message;
+                }
+                finally { _con.Close(); }
+            }
         }
 
         private void IncreaseTelemetryData(int id)
@@ -118,11 +213,15 @@ namespace PracaInzynierska
             }
         }
 
+        
+
         [RelayCommand]
         public async Task StartInstallationProcess()
         {
+            EnableControls = false;
             bool installed = false;
             ProgressText = string.Empty;
+            string notInstalled = "";
             for (int i = 0; i < SelectedSoftwareItems.Count; i++)
             {
                 Version newVersion = new Version(SelectedSoftwareItems[i].Version);
@@ -141,6 +240,8 @@ namespace PracaInzynierska
 
                 if (versionComparison < 0)
                 {
+                    ProgressText += "------------------------Proces instalacji " + SelectedSoftwareItems[i].Name + "------------------------\n";
+
                     OperationInProgress = true;
                     
                     Task<bool> downloading = DownloadInstaller(SelectedSoftwareItems[i].DownloadLink, SelectedSoftwareItems[i].Name);
@@ -153,7 +254,7 @@ namespace PracaInzynierska
                         continue;
                     }
 
-                    ProgressText += $"Instaluje {SelectedSoftwareItems[i].Name}...\n";
+                    ProgressText += $"\tPróbuję zainstalować {SelectedSoftwareItems[i].Name}...\n";
 
                     Task<bool> installing;
 
@@ -171,26 +272,29 @@ namespace PracaInzynierska
 
                     await Task.Delay(1000); //Czekanie na wpisanie zmian do rejestru itd. Program będzie bez tego zainstalowany, ale lista się nie odświeży.
 
+
                     OperationInProgress = false;
                     afterInstalling = true;
                     if (installResult)
                     {
-                        ProgressText += $"Poprawnie zainstalowano program {SelectedSoftwareItems[i].Name} w wersji {SelectedSoftwareItems[i].Version}\n";
+                        ProgressText += $" + Poprawnie zainstalowano program {SelectedSoftwareItems[i].Name} w wersji {SelectedSoftwareItems[i].Version}\n";
                         installed = true;
                         IncreaseTelemetryData(SelectedSoftwareItems[i].SoftwareId);
                     }
                     else
                     {
-                        ProgressText += $"Błąd podczas instalacji {SelectedSoftwareItems[i].Name}\n";
+                        notInstalled += "\t" + SelectedSoftwareItems[i].Name + "\n";
+                        ProgressText += $"\tBłąd podczas instalacji {SelectedSoftwareItems[i].Name}\n";
                     }
+                    ProgressText += "------------------------Koniec instalacji " + SelectedSoftwareItems[i].Name + "------------------------\n";
                 }
                 else if (versionComparison > 0)
                 {
-                    ProgressText += $"Program {SelectedSoftwareItems[i].Name} posiada wersję nowszą niż tą w bazie danych!\n";
+                    ProgressText += $"-Program {SelectedSoftwareItems[i].Name} posiada wersję nowszą niż tą w bazie danych!\n";
                 }
                 else
                 {
-                    ProgressText += $"Program {SelectedSoftwareItems[i].Name} posiada już najnowszą wersję!\n";
+                    ProgressText += $"-Program {SelectedSoftwareItems[i].Name} posiada już najnowszą wersję!\n";
                 }
             }
             if (installed) //Do poprawy
@@ -199,8 +303,13 @@ namespace PracaInzynierska
             }
             else
             {
-                //ProgressText = "Nie zainstalowano programów...";
+                if(notInstalled != String.Empty)
+                {
+                    ProgressText += " ! Nie zainstalowano poniższych programów z powodu błędów: ! \n";
+                    ProgressText += notInstalled;
+                }
             }
+            EnableControls = true;
         }
 
         public async Task<bool> DownloadInstaller(string link, string name)
@@ -228,7 +337,7 @@ namespace PracaInzynierska
             }
             else
             {
-                ProgressText += $"Plik instalacyjny dla {name} już istnieje...\n";
+                ProgressText += $"\tPlik instalacyjny dla {name} już istnieje...\n";
                 return true;
             }
         }
@@ -242,23 +351,26 @@ namespace PracaInzynierska
 
                 processStartInfo = new ProcessStartInfo();
                 processStartInfo.CreateNoWindow = true;
-                processStartInfo.UseShellExecute = false;
                 processStartInfo.Arguments = parameters;
                 processStartInfo.FileName = path;
+
+                processStartInfo.RedirectStandardError = true;
+                processStartInfo.RedirectStandardOutput = true;
 
                 process = new Process();
                 process.StartInfo = processStartInfo;
                 process.Start();
 
                 await process.WaitForExitAsync();
-                if (process.HasExited)
+                string appName = new DirectoryInfo(path).Name;
+                if (InstalledApplications.FindApp(appName, AppList) != null)
                 {
-                    if(process.ExitCode == 0)
-                        return true;
-                    else return false;
+                    return true;
                 }
                 else
+                {
                     return false;
+                }
             }
             catch (Exception ex)
             {
@@ -285,8 +397,6 @@ namespace PracaInzynierska
             {
                 AddProgramEntries(k);
             }
-
-
         }
 
         private void AddProgramEntries(RegistryKey key)
@@ -359,13 +469,9 @@ namespace PracaInzynierska
             if(parameter != String.Empty)
             {
                 parameter = parameter.ToLower();
-                foreach (SoftwareItem item in SoftwareItems)
+                foreach (SoftwareItem item in CurrentSoftwareItems)
                 {
                     if (item.Name.ToLower().Contains(parameter))
-                    {
-                        item.IsHidden = false;
-                    }
-                    else if (item.Category.ToLower().Contains(parameter))
                     {
                         item.IsHidden = false;
                     }
@@ -381,7 +487,7 @@ namespace PracaInzynierska
             }
             else
             {
-                foreach (SoftwareItem item in SoftwareItems)
+                foreach (SoftwareItem item in CurrentSoftwareItems)
                 {
                     item.IsHidden = false;
                 }
